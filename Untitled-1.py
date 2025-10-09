1e51e716063901b95a9c99ace2716f96b0f34adf
kondratev17102009-tech <kondratev.17.10.2009@gmail.com>
2025-10-09 14:03:27 +0300

    09.10.2025 Python

---
+++ Untitled-1.py
@@ -0,0 +1,39 @@
+#users = {"Tom", "Bob", "Alice", "Tom"}
+#print(users)    # {"Alice", "Bob", "Tom"}
+ 
+#people = ["sfsf"]
+#users = set(people)
+#print(users)
+
+
+#users = set()
+
+#users = {"Tom", "Bob", "Alice"}
+#print(len(users))  
+
+#users = set()
+#users.add("Sam")
+#print(users)
+
+
+#users = {"fdsfs", "fdfs"}
+#
+#user = "fdsfs"
+#if users in users:
+#    users.remove(users)
+#    print(users)
+
+
+
+#users = {"Tom", "Bob", "Alice"}
+# 
+#users.discard("Tim")    # элемент "Tim" отсутствует, и метод ничего не делает
+#print(users)    #  {"Tom", "Bob", "Alice"}
+# 
+#users.discard("Tom")    # элемент "Tom" есть, и метод удаляет элемент
+#print(users)    #  {"Bob", "Alice"}
+
+
+	
+users.clear()
+
---
+++ main.py
@@ -0,0 +1,28 @@
+import shaps
+import turtle
+
+Harry = turtle.Turtle()
+Screen = Harry.getscreen()
+shaps.square(Harry, 0, 0, 50)
+
+Screen.setup(1000, 100)
+Screen.bgpic("test.png")
+Screen.title("Олег")
+input()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
